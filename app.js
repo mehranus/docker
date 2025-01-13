41df80e0e72be3314970a3d06561e34138ddac17
mehran feriduni <m3hr4nus@gmail.com>
2025-01-13 13:27:20 +0330

    run docker project

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app=express()
+const port=3000
+
+app.get('/',(req,res)=>{
+   res.send("Hello Docker")
+})
+
+
+app.listen(port,()=>{
+  console.log(`run server in port :${port}`)
+})
