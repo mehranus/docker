ea0f4f1d4d97f5b43ab67656d8b6caeef44bb6d4
mehran feriduni <m3hr4nus@gmail.com>
2025-01-21 17:44:59 +0330

    remove countaner

--- python/app.py
+++ python/app.py
@@ -2,6 +2,7 @@ from random import randint
 
 min_random=int(input("please input min number:"))
 max_random=int(input("please input max number:"))
+#docker removie contaner
 
 
 if(max_random < min_random):
