e5fbbc63c56153b4013073eb8db72c8a3ced3cbe
mehran feriduni <m3hr4nus@gmail.com>
2025-01-21 17:31:59 +0330

    docker -ai and -it

---
+++ python/app.py
@@ -0,0 +1,11 @@
+from random import randint
+
+min_random=int(input("please input min number:"))
+max_random=int(input("please input max number:"))
+
+
+if(max_random < min_random):
+  print("invalid input - shatting down ...")
+else:
+  rnd_int=randint(min_random,max_random)
+  print(rnd_int)  
